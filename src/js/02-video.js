530db4de988af1906d0e33b31e72027d6c6ee1a3
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-02-11 14:53:11 +0200

    add 1 & 2 hw

---
+++ src/js/02-video.js
@@ -0,0 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+const currentTime = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(currentTime, 1000));
