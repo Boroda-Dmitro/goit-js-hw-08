8e63609de6336dca5d74143ae207ea196d5a4517
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-02-11 16:25:25 +0200

    add 3 hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const pageForm = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+const formDate = {};
+
+const onInputForm = e => {
+  formDate[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formDate));
+};
+
+const onSubmitForm = e => {
+  e.preventDefault();
+  const submitValue = {
+    email: pageForm.email.value,
+    message: pageForm.message.value,
+  };
+  if (submitValue.email !== '' && submitValue.message !== '') {
+    console.log(submitValue);
+  }
+
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+const addDateToLocalStorage = pageForm.form.addEventListener(
+  'input',
+  throttle(onInputForm, 500)
+);
+const submitForem = pageForm.form.addEventListener('submit', onSubmitForm);
+
+const formInputValue = () => {
+  const getDateFromLocalStorage = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  if (getDateFromLocalStorage) {
+    pageForm.email.value = getDateFromLocalStorage.email;
+    pageForm.message.value = getDateFromLocalStorage.message;
+  }
+};
+
+formInputValue();
